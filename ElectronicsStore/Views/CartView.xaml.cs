32b0d61e8d79fefada6de5f2eaf4e260a9b78840
Rainbow Dash <arbuzz.tf2.ru@yandex.ru>
2025-07-02 23:50:47 +0300

    Переделан интерфейс под более современный стиль, исправлены некоторые логические ошибки

--- ElectronicsStore/Views/CartView.xaml.cs
+++ ElectronicsStore/Views/CartView.xaml.cs
@@ -39,30 +39,30 @@ namespace ElectronicsStore.Views
         {
             _allProducts = new List<Product>
             {
-                new Product { Id = 1, Name = "Ноутбук ASUS ROG", Price = 696.96, ImagePath = "pack://application:,,,/images/laptop.jpg" },
-                new Product { Id = 2, Name = "Смартфон iPhone 13", Price = 799.99, ImagePath = "pack://application:,,,/images/phone.jpg" },
-                new Product { Id = 3, Name = "Наушники Sony WH-1000XM4", Price = 349.98, ImagePath = "pack://application:,,,/images/headphones.jpg" },
-                new Product { Id = 4, Name = "Монитор Dell 27\"", Price = 255.19, ImagePath = "pack://application:,,,/images/monitor.jpg" },
+                new Product { Id = 1, Name = "Ноутбук ASUS ROG", Price = 55696.96, ImagePath = "pack://application:,,,/images/laptop.jpg" },
+                new Product { Id = 2, Name = "Смартфон iPhone 13", Price = 79900.99, ImagePath = "pack://application:,,,/images/phone.jpg" },
+                new Product { Id = 3, Name = "Наушники Sony WH-1000XM4", Price = 349244.98, ImagePath = "pack://application:,,,/images/headphones.jpg" },
+                new Product { Id = 4, Name = "Монитор Dell 27", Price = 25500.19, ImagePath = "pack://application:,,,/images/monitor.jpg" },
                 new Product { Id = 5, Name = "Клавиатура Logitech MX Keys", Price = 99.99, ImagePath = "pack://application:,,,/images/keyboard.jpg" },
-                new Product { Id = 6, Name = "Мышь Razer DeathAdder", Price = 69.25, ImagePath = "pack://application:,,,/images/mouse.jpg" },
-                new Product { Id = 7, Name = "Планшет Samsung Galaxy Tab S7", Price = 649.11, ImagePath = "pack://application:,,,/images/tablet.jpg" },
-                new Product { Id = 8, Name = "Фитнес-браслет Xiaomi Mi Band 6", Price = 49.10, ImagePath = "pack://application:,,,/images/band.jpg" },
-                new Product { Id = 9, Name = "SSD Samsung 980 Pro 1TB", Price = 149.34, ImagePath = "pack://application:,,,/Images/ssd.jpg" },
-                new Product { Id = 10, Name = "Жесткий диск Seagate BarraCuda 2TB", Price = 69.99, ImagePath = "pack://application:,,,/Images/hdd.jpg", },
-                new Product { Id = 11, Name = "Материнская плата ASUS ROG Strix B550-F", Price = 189.99, ImagePath = "pack://application:,,,/Images/motherboard.jpg", },
-                new Product { Id = 12, Name = "Блок питания Corsair RM750x 750W", Price = 129.49, ImagePath = "pack://application:,,,/Images/psu.jpg",  },
-                new Product { Id = 13, Name = "Корпус NZXT H510 Black", Price = 89.99, ImagePath = "pack://application:,,,/Images/case.jpg", },
-                new Product { Id = 14, Name = "Охлаждение CPU Noctua NH-D15", Price = 91.19, ImagePath = "pack://application:,,,/Images/cooler.jpg",  },
-                new Product { Id = 15, Name = "Оперативная память Corsair Vengeance 32GB", Price = 129.1, ImagePath = "pack://application:,,,/Images/ram.jpg",  },
-                new Product { Id = 16, Name = "Веб-камера Logitech C920 HD Pro", Price = 79.46, ImagePath = "pack://application:,,,/Images/webcam.jpg", },
-                new Product { Id = 17, Name = "Микрофон Blue Yeti Blackout", Price = 129.23, ImagePath = "pack://application:,,,/Images/mic.jpg",  },
-                new Product { Id = 18, Name = "Игровой коврик Razer Goliathus Extended", Price = 15.69, ImagePath = "pack://application:,,,/Images/mat.jpg", },
-                new Product { Id = 19, Name = "МФУ HP LaserJet Pro MFP", Price = 288.69, ImagePath = "pack://application:,,,/Images/printer.jpg", },
-                new Product { Id = 20, Name = "Роутер ASUS RT-AX86U", Price = 249.99, ImagePath = "pack://application:,,,/Images/router.jpg", },
-                new Product { Id = 21, Name = "Оперативная память Fujitsu RAM DDR4", Price = 120.94, ImagePath = "pack://application:,,,/Images/ram1.jpg",  },
-                new Product { Id = 22, Name = "Ноутбук honor magixbook", Price = 884.919, ImagePath = "pack://application:,,,/images/laptop1.jpg" },
-                new Product { Id = 23, Name = "Смартфон xiaomi redmi note 14", Price = 359.39, ImagePath = "pack://application:,,,/images/phone1.jpg" },
-                new Product { Id = 24, Name = "Клавиатура redsquare rgb", Price = 59.59, ImagePath = "pack://application:,,,/images/keyboard1.jpg" },
+                new Product { Id = 6, Name = "Мышь Razer DeathAdder", Price = 1690.25, ImagePath = "pack://application:,,,/images/mouse.jpg" },
+                new Product { Id = 7, Name = "Планшет Samsung Galaxy Tab S7", Price = 64900.11, ImagePath = "pack://application:,,,/images/tablet.jpg" },
+                new Product { Id = 8, Name = "Фитнес-браслет Xiaomi Mi Band 6", Price = 1490.10, ImagePath = "pack://application:,,,/images/band.jpg" },
+                new Product { Id = 9, Name = "SSD Samsung 980 Pro 1TB", Price = 5490.34, ImagePath = "pack://application:,,,/Images/ssd.jpg" },
+                new Product { Id = 10, Name = "Жесткий диск Seagate BarraCuda 2TB", Price = 1690.99, ImagePath = "pack://application:,,,/Images/hdd.jpg", },
+                new Product { Id = 11, Name = "Материнская плата ASUS ROG Strix B550-F", Price = 1829.99, ImagePath = "pack://application:,,,/Images/motherboard.jpg", },
+                new Product { Id = 12, Name = "Блок питания Corsair RM750x 750W", Price = 12912.49, ImagePath = "pack://application:,,,/Images/psu.jpg",  },
+                new Product { Id = 13, Name = "Корпус NZXT H510 Black", Price = 8944.99, ImagePath = "pack://application:,,,/Images/case.jpg", },
+                new Product { Id = 14, Name = "Охлаждение CPU Noctua NH-D15", Price = 9125.19, ImagePath = "pack://application:,,,/Images/cooler.jpg",  },
+                new Product { Id = 15, Name = "Оперативная память Corsair Vengeance 32GB", Price = 1269.1, ImagePath = "pack://application:,,,/Images/ram.jpg",  },
+                new Product { Id = 16, Name = "Веб-камера Logitech C920 HD Pro", Price = 7900.46, ImagePath = "pack://application:,,,/Images/webcam.jpg", },
+                new Product { Id = 17, Name = "Микрофон Blue Yeti Blackout", Price = 12976.23, ImagePath = "pack://application:,,,/Images/mic.jpg",  },
+                new Product { Id = 18, Name = "Игровой коврик Razer Goliathus Extended", Price = 151.69, ImagePath = "pack://application:,,,/Images/mat.jpg", },
+                new Product { Id = 19, Name = "МФУ HP LaserJet Pro MFP", Price = 28802.69, ImagePath = "pack://application:,,,/Images/printer.jpg", },
+                new Product { Id = 20, Name = "Роутер ASUS RT-AX86U", Price = 2495.99, ImagePath = "pack://application:,,,/Images/router.jpg", },
+                new Product { Id = 21, Name = "Оперативная память Fujitsu RAM DDR4", Price = 5202.94, ImagePath = "pack://application:,,,/Images/ram1.jpg",  },
+                new Product { Id = 22, Name = "Ноутбук honor magixbook", Price = 88400.919, ImagePath = "pack://application:,,,/images/laptop1.jpg" },
+                new Product { Id = 23, Name = "Смартфон xiaomi redmi note 14", Price = 13590.39, ImagePath = "pack://application:,,,/images/phone1.jpg" },
+                new Product { Id = 24, Name = "Клавиатура redsquare rgb", Price = 544322125123559.59, ImagePath = "pack://application:,,,/images/keyboard1.jpg" },
             };
         }
 
@@ -95,7 +95,7 @@ namespace ElectronicsStore.Views
         private void UpdateCartSummary()
         {
             ItemsCountText.Text = _cartProducts.Count.ToString();
-            TotalPriceText.Text = _cartProducts.Sum(p => p.Price).ToString("C");
+            TotalPriceText.Text = _cartProducts.Sum(p => p.Price).ToString();
         }
 
 
--- ElectronicsStore/Views/MainStoreWindow.xaml.cs
+++ ElectronicsStore/Views/MainStoreWindow.xaml.cs
@@ -21,28 +21,95 @@ namespace ElectronicsStore.Views
     /// </summary>
     public partial class MainStoreWindow : Window
     {
+        private bool _isMaximized = false;
         private string _username;
-
-
-
 
         public MainStoreWindow(string username)
         {
             InitializeComponent();
-            //    ProductsButton.Click += (s, e) => MainFrame.Navigate(new ProductsView(username));
-            //    CartButton.Click += (s, e) => MainFrame.Navigate(new CartView(username));
             _username = username;
-            MainFrame.Navigate(new ProductsView(_username));
+            this.Loaded += (s, e) =>
+            {
+                ProductsButton_Click(null, null);
+
+            };
+
+
+
+            this.MouseLeftButtonDown += (s, e) =>
+            {
+                if (e.GetPosition(this).Y <= 60)DragMove();
+                if (e.ClickCount == 2 && e.GetPosition(this).Y <= 60)ToggleMaximize();
+            };
+        }
+
+
+
+
+
+
+        private void ActivateButton(Button button)
+        {
+
+            if (ProductsButton == null || CartButton == null) return;
+            var productsUnderline = (Rectangle)ProductsButton.Template?.FindName("underline", ProductsButton);
+            var cartUnderline = (Rectangle)CartButton.Template?.FindName("underline", CartButton);
+            if (productsUnderline != null) productsUnderline.Opacity = button == ProductsButton ? 1 : 0;
+
+            if (cartUnderline != null)cartUnderline.Opacity = button == CartButton ? 1 : 0;
+
+
         }
 
         private void ProductsButton_Click(object sender, RoutedEventArgs e)
         {
+            ActivateButton(ProductsButton);
             MainFrame.Navigate(new ProductsView(_username));
         }
-
         private void CartButton_Click(object sender, RoutedEventArgs e)
         {
+            ActivateButton(CartButton);
             MainFrame.Navigate(new CartView(_username));
+        }
+
+
+
+        private void MinimizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+
+
+        private void ToggleMaximize()
+        {
+            _isMaximized = !_isMaximized;
+
+            if (_isMaximized)
+            {
+                WindowState = WindowState.Maximized;
+                MaximizeButton.Content = "❒";
+                Margin = new Thickness(0);
+            }
+            else
+            {
+                WindowState = WindowState.Normal;
+                MaximizeButton.Content = "□";
+                Margin = new Thickness(10);
+            }
+
+
+
+        }
+
+        private void MaximizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            ToggleMaximize();
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
         }
     }
 }
